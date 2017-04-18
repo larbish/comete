7e63ba6a01d1a64e1a49a1b8c0248af50ebff660
bleproux <leproux.baptiste@gmail.com>
2017-04-18 23:34:07 +0200

    infos: Page almost done

--- app/navigation/navigation-controller.js
+++ app/navigation/navigation-controller.js
@@ -1,11 +1,13 @@
 angular.module('app')
 
-	.controller('NavigationController', function (AppService) {
+	.controller('NavigationController', function (AppService, $rootScope) {
 		var vm = this;
 
 		vm.isNavOpen = AppService.isNavOpen();
 
 		vm.toggleNav = function () {
 			vm.isNavOpen = AppService.toggleNav();
+			if (vm.isNavOpen) $rootScope.noscroll = 'noscroll';
+			else $rootScope.noscroll = '';
 		}
 	});
