93d3e6f1ea970ae44fb82bf2bce061ce30d57ae3
Baptiste Leproux <baptiste.leproux@neo9.fr>
2017-04-27 13:57:01 +0200

    prog: Change artist img organization

--- app/programmation/programmation-controller.js
+++ app/programmation/programmation-controller.js
@@ -93,6 +93,11 @@ angular.module('app')
 				'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
 				'img': imgPath + 'MoneyP.png'
 			},
+			'paradis': {
+				'name': 'Paradis',
+				'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+				'img': imgPath + 'Paradis.png'
+			},
 			'rituel': {
 				'name': 'Rituel',
 				'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
